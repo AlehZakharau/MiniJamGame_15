25bd6bef39a99f9f874672a57823829eb1808e24
Aleh Zaharau <38154177+AlehZakharau@users.noreply.github.com>
2023-03-03 11:27:03 -0500

    Rotate turret

--- main/aim.go
+++ main/aim.go
@@ -1,29 +1,21 @@
-components {
-  id: "turret"
-  component: "/code/turret.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
 embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/assets/items.atlas\"\n"
-  "default_animation: \"turret\"\n"
+  "default_animation: \"target\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
+  "size {\n"
+  "  x: 25.0\n"
+  "  y: 25.0\n"
+  "  z: 0.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "size_mode: SIZE_MODE_MANUAL\n"
   ""
   position {
-    x: 0.0
-    y: 0.0
+    x: 320.0
+    y: -156.0
     z: 0.0
   }
   rotation {
@@ -31,10 +23,5 @@ embedded_components {
     y: 0.0
     z: 0.0
     w: 1.0
-  }
-  scale {
-    x: 0.14
-    y: 0.14
-    z: 0.14
   }
 }
