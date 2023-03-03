3e7a2be6c9fe489649522c3b092bb5c33e5326bc
Aleh Zaharau <38154177+AlehZakharau@users.noreply.github.com>
2023-03-03 09:08:36 -0500

    First Map

---
+++ main/turret.go
@@ -0,0 +1,40 @@
+components {
+  id: "turret"
+  component: "/code/turret.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/assets/items.atlas\"\n"
+  "default_animation: \"turret\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: -47.0
+    y: -178.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  scale {
+    x: 0.14
+    y: 0.14
+    z: 0.14
+  }
+}
