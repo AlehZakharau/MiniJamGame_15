9abf354fe9d350bc68d674d81466e12d40b31e74
Aleh Zaharau <38154177+AlehZakharau@users.noreply.github.com>
2023-03-03 20:07:35 -0500

    Bullet launch, targeting and hit

---
+++ main/bullet_factory.go
@@ -0,0 +1,15 @@
+components {
+  id: "factory_bullet"
+  component: "/main/factory_bullet.factory"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
